6d1e85363f4f864d51df2fc51c8399940cf3fcd4
JustAMan22 <mrginod22@yahoo.com>
2023-06-16 20:57:01 -0500

    Fixed delete a spot image

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -516,14 +516,17 @@ router.delete("/:spotId/images/:imageId", requireAuth, async (req, res) => {
   const imageId = req.params.imageId;
   const spot = await Spot.findByPk(spotId);
   const spotImage = await SpotImage.findByPk(imageId);
-  if (!spotImage) {
-    return res.status(404).json({ message: "Spot Image couldn't be found" });
-  }
-  if (spot.ownerId !== userId) {
-    return res.status(403).json({ message: "Forbidden" });
-  }
-  await spotImage.destroy();
-  return res.status(200).json({ message: "Successfully deleted" });
+
+  if (spot) {
+    if (spotImage) {
+      if (spot.ownerId === userId) {
+        await spotImage.destroy();
+        return res.status(200).json({ message: "Successfully deleted" });
+      } else return res.status(403).json({ message: "Forbidden" });
+    } else
+      return res.status(404).json({ message: "Spot Image couldn't be found" });
+  } else res.status(404).json({ mesasge: "Spot couldn't be found" });
+
 });
 
 module.exports = router;
